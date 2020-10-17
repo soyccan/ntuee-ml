fdab4f24b2fc5bd2d901ee06e9ff44bac0551be5
soyccan <soyccan@gmail.com>
2020-10-18 02:25:57 +0800

    remove pyplot dependency

--- best-test.py
+++ best-test.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import sklearn as sk
 import sklearn.linear_model
 import sklearn.feature_selection
--- main-test.py
+++ main-test.py
@@ -1,7 +1,6 @@
 import sys
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import joblib
 
 class Dataset:
