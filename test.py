f37276d5a8e4047c52a02b27d3a49b1b20145cc5
soyccan <soyccan@gmail.com>
2020-11-15 01:19:56 +0800

    final

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+import sys
+
 import numpy as np
 import pandas as pd
 from tensorflow import keras
-import os
 
-X_train = np.load('X_train.npy').astype(np.float32)
-y_train = np.load('y_train.npy').astype(np.float32)
-X_test = np.load('X_test.npy').astype(np.float32)
-n_train = X_train.shape[0]
-n_test = X_test.shape[0]
+test_path = sys.argv[1]
+pred_path = sys.argv[2]
 
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+n_test = len(os.listdir(test_path))
+X_test = np.array([
+    np.array(Image.open(os.path.join(test_path, '{:05}.jpg'.format(i))))
+    for i in range(n_test)
+]).reshape((-1, 48, 48, 1))
 
 model = keras.models.load_model('1/9.hdf5')
 
-# print(model.evaluate(X_train, y_train))
-
 y_test = model.predict_classes(X_test)
-pred = pd.DataFrame({
-    'id': np.arange(n_test),
-    'label': y_test
-})
-pred.to_csv('submission.csv', index=False)
+pred = pd.DataFrame({'id': np.arange(n_test), 'label': y_test})
+pred.to_csv(pred_path, index=False)
--- train.py
+++ train.py
@@ -8,54 +8,24 @@ from tensorflow.keras.regularizers import *
 from tensorflow.keras.optimizers import *
 
 import os.path
+import sys
 
+train_path = sys.argv[1]
+label_path = sys.argv[2]
 
-class MyThresholdCallback(tf.keras.callbacks.Callback):
-    def __init__(self, threshold):
-        super(MyThresholdCallback, self).__init__()
-        self.threshold = threshold
-
-    def on_epoch_end(self, epoch, logs=None):
-        val_acc = logs["val_acc"]
-        if val_acc >= self.threshold:
-            self.model.stop_training = True
-
-
-# TODO: !!!set to false before submitting!!!
-USE_SAVED_MODEL = True
-
-if USE_SAVED_MODEL:
-    X_train = np.load('X_train.npy').astype(np.float32)
-    y_train = np.load('y_train.npy').astype(np.float32)
-    X_test = np.load('X_test.npy').astype(np.float32)
-    n_train = X_train.shape[0]
-    n_test = X_test.shape[0]
-else:
-    n_train = len(os.listdir('train/train')) - 1
-    n_test = len(os.listdir('test/test'))
-
-    label = pd.read_csv('train/train/label.csv')
-    y_train = pd.get_dummies(label['label'], columns=['label'])  # one-hot
-    X_train = np.array([
-        np.array(Image.open('train/train/{:05}.jpg'.format(i)))
-        for i in range(n_train)]
-    ).reshape((-1, 48, 48, 1))
-    X_test = np.array([
-        np.array(Image.open('test/test/{:05}.jpg'.format(i)))
-        for i in range(n_test)]
-    ).reshape((-1, 48, 48, 1))
-
-    # np.save('X_train.npy', X_train)
-    # np.save('X_test.npy', X_test)
-    # np.save('y_train.npy', y_train)
+label = pd.read_csv(label_path)
+y_train = pd.get_dummies(label['label'], columns=['label'])  # one-hot
+n_train = label.shape[0]
+X_train = np.array([
+    np.array(Image.open(os.path.join(train_path, '{:05}.jpg'.format(i))))
+    for i in range(n_train)
+]).reshape((-1, 48, 48, 1))
 
 print('y_train', y_train.shape, '\n', y_train)
 print('X_train', X_train.shape)
-print('X_test', X_test.shape)
-
 
 # set GPU to use
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 tf.config.threading.set_inter_op_parallelism_threads(1)
 tf.config.threading.set_intra_op_parallelism_threads(4)
@@ -65,20 +35,20 @@ if gpus:
     # Restrict TensorFlow to only use the first GPU
     try:
         tf.config.set_visible_devices(gpus[0], 'GPU')
-        tf.config.experimental.set_memory_growth(gpus[0], False)  # grow as needed
+        tf.config.experimental.set_memory_growth(gpus[0],
+                                                 False)  # grow as needed
         tf.config.set_soft_device_placement(False)  # don't use CPU
         tf.config.experimental.set_virtual_device_configuration(
             gpus[0],
-            [tf.config.experimental.VirtualDeviceConfiguration(
-                memory_limit=2000)  # set < 2G to survive in CSIE
-            ]
-        )
+            [
+                tf.config.experimental.VirtualDeviceConfiguration(
+                    memory_limit=2000)  # set < 2G to survive in CSIE
+            ])
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
     except RuntimeError as e:
         # Visible devices must be set before GPUs have been initialized
         print(e)
-
 
 old_model_path = None
 
@@ -144,28 +114,21 @@ else:
         loss='categorical_crossentropy',
         metrics=['categorical_accuracy'])
 
-train_history = model.fit(
-    x=X_train,
-    y=y_train,
-    epochs=1000,
-    batch_size=64,
-    validation_split=0.1,
-    shuffle=True,
-    verbose=1,
-    # use_multiprocessing=True,
-    # workers=24,
-    callbacks=[
-        keras.callbacks.ModelCheckpoint(
-            "1/9.hdf5",
-            monitor='val_categorical_accuracy',
-            verbose=1,
-            save_best_only=True,
-            mode='max',
-            save_freq='epoch'),
-        # keras.callbacks.EarlyStopping(
-        #     monitor="val_accuracy",
-        #     baseline=0.589,
-        #     min_delta=0.01,
-        #     restore_best_weights=False,
-        # )
-    ])
+    train_history = model.fit(x=X_train,
+                              y=y_train,
+                              epochs=1000,
+                              batch_size=64,
+                              validation_split=0.1,
+                              shuffle=True,
+                              verbose=1,
+                              use_multiprocessing=True,
+                              workers=24,
+                              callbacks=[
+                                  keras.callbacks.ModelCheckpoint(
+                                      "/tmp/9.hdf5",
+                                      monitor='val_categorical_accuracy',
+                                      verbose=1,
+                                      save_best_only=True,
+                                      mode='max',
+                                      save_freq='epoch'),
+                              ])
